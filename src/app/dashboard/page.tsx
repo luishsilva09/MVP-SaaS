6af5aa2de668223eff73e1438ca2efc4b9d0d53d
Luis Henrique <luishsilva09@gmail.com>
2025-05-15 23:19:34 -0300

    Fix: importacao incorreta

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth/next'
 import { redirect } from 'next/navigation'
 import Header from '../components/header'
 import Dashboard from './DashboardClient'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/utils/authOptions'
 
 export default async function DashboardPage() {
     const session = await getServerSession(authOptions)
