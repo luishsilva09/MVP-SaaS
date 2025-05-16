15cb06112d1ba4b23f1ad00a16829565b9b783a4
Luis Henrique <luishsilva09@gmail.com>
2025-05-15 22:06:05 -0300

    Feat: pagina inicial de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { FormLogin } from "./components/loginForm";
+
+export default function Login() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <body className="antialiased bg-slate-200">
+      <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+        <h1 className="text-4xl font-medium">Login</h1>
+        <div className="my-5">
+        </div>
+        <FormLogin />
+      </div>
+    </body>
   );
 }
